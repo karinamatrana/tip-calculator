870b1490b4180ad3efb88a538478b0f69d3ea10d
karinamatrana <karina.matrana@gmail.com>
2018-07-20 14:40:40 +0300

    Fixed bug

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-document.getElementById("container").onchange = function() {
+var bill = function() {
     var bill = document.getElementById("billTotal").value;
     // console.log(bill);
     var tipPercent = document.getElementById("tipInput").value;
     // console.log(tipPercent);
     var split = document.getElementById("splitInput").value;
-    // console.log(bill, tipPercent, split);
+    console.log(bill, tipPercent, split);
     var tipValue = bill * (tipPercent/100);
-    
+    console.log("tip:" + tipValue);
     var newBillEach = (parseInt(bill) + parseInt(tipValue)) / split;
-
+    console.log("cost nou: " + newBillEach);
     var tipEach = tipValue / split;
-
+    console.log("bacsis/persoana: " + tipEach);
     document.getElementById("newBill").innerHTML = "RON " + newBillEach.toFixed(2);
     document.getElementById("tipPerPerson").innerHTML = "RON " + tipEach.toFixed(2);
     
 }
+
+document.getElementById("billTotal").onchange = bill;
+document.getElementById("tipInput").onchange = bill;
+document.getElementById("billTotal").onchange = bill;
+
